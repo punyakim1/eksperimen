facbe982e816925842c554cc622097290329ab45
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2023-03-14 22:54:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,20 @@
 import pyrogram
+import config
+from pyrogram import filters
 
-# Initialize the client
+# ini buat namanin app nya
 app = pyrogram.Client(
-    ":Rexa:",
+    ":kim:",
     config.API_ID,
     config.API_HASH,
     bot_token=config.BOT_TOKEN,
 )
 
-# Start the client
-app.start()
+# ini buat start nya
 
-# Define a function to handle the /start command
-@app.on_message(pyrogram.Filters.command("start"))
-def start(client, message):
-    message.reply("Welcome to the Guess Bot game!\n\nI'm thinking of a number between 1 and 10. Can you guess it?")
+@app.on_message(filters.command("mulai"))
+async start chat_actions(_, message):
+    await app.send_message(message.chat.id, "halo bang, makasih udah start bot ini awoakwoak")
 
-# Define a function to handle the user's guess
-@app.on_message()
-def guess(client, message):
-    # Generate a random number between 1 and 10
-    secret_number = random.randint(1, 10)
-    
-    # Get the user's guess
-    user_guess = int(message.text)
-    
-    # Check if the user's guess is correct
-    if user_guess == secret_number:
-        message.reply("You guessed it! The number was {}".format(secret_number))
-    else:
-        message.reply("Sorry, that's not it. The number was {}".format(secret_number))
-
-# Run the client
+print('dah jalan')
 app.run()
